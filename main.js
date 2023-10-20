f0e5095e3db80b91fc123347aa4221131a4fc904
seif samir <117075048+seif-samir89@users.noreply.github.com>
2023-10-20 19:39:07 +0300

    Pepsi

---
+++ main.js
@@ -0,0 +1,10 @@
+let  pepsican = document.querySelector('.pepsican');
+let container = document.querySelector('.container');
+
+function Pepsi(cans){
+    pepsican.src = cans;
+}
+function colors(color){
+    container.style.background = color;
+
+}
